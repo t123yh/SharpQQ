bd391cdbc63455f687d3fa84bb17dd6cb588f378
t123yh <t123yh@outlook.com>
2018-01-06 22:28:57 +0800

    Add QQ number in AuthInfo for validation purpose.

--- SharpQQLib/Protocol/Msf/AccountAuthInfo.cs
+++ SharpQQLib/Protocol/Msf/AccountAuthInfo.cs
@@ -2,6 +2,8 @@ namespace SharpQQ.Protocol.Msf
 {
     public class AccountAuthInfo
     {
+        public long QQNumber { get; set; }
+        
         public byte[] A2 { get; set; }
 
         public byte[] D2 { get; set; }
