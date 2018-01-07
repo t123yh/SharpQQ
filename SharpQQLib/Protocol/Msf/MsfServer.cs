896a2fc7a3f33cac1f692069dd9c8c2b8c8fcd2f
t123yh <t123yh@outlook.com>
2018-01-07 22:52:01 +0800

    Implement MsfServer basic functions.

--- SharpQQLib/Protocol/Msf/MsfServer.cs
+++ SharpQQLib/Protocol/Msf/MsfServer.cs
@@ -3,58 +3,35 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Collections.Concurrent;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using SharpQQ.Protocol.Msf.Packets;
 using Nito.AsyncEx;
+using Org.BouncyCastle.Asn1.Cms;
+using SharpQQ.Utils;
 
 namespace SharpQQ.Protocol.Msf
 {
     public class MsfServer
     {
-        const string DefaultAddress = "msfxg.3g.qq.com";
-        const int DefaultPort = 14000;
+        private const string DefaultAddress = "msfxg.3g.qq.com";
+        private const int DefaultPort = 14000;
 
-        static readonly ReadOnlyCollection<string> NoEncryptionOperations = new ReadOnlyCollection<string>(new string[]{
-            "heartbeat.ping",
-            "heartbeat.alive",
-            "client.correcttime"});
+        public bool Connected => this._baseClient != null && this._baseClient.Connected;
 
-        static readonly ReadOnlyCollection<string> ZeroEncryptionOperations = new ReadOnlyCollection<string>(new string[]{
-            "login.auth",
-            "wtlogin.login",
-            "login.chguin",
-            "grayuinpro.check",
-            "wtlogin.name2uin",
-            "wtlogin.exchange_emp",
-            "wtlogin.trans_emp",
-            "account.requestverifywtlogin_emp",
-            "account.requestrebindmblwtLogin_emp",
-            "connauthsvr.get_app_info_emp",
-            "connauthsvr.get_auth_api_list_emp",
-            "connauthsvr.sdk_auth_api_emp"});
-
-
-        public bool Connected
-        {
-            get
-            {
-                return this._baseClient != null && this._baseClient.Connected;
-            }
-        }
-
-        Dictionary<int, MsfTask> _taskList = new Dictionary<int, MsfTask>();
+        private readonly Dictionary<int, MsfTask> _taskList = new Dictionary<int, MsfTask>();
+        private readonly AsyncManualResetEvent _connectedEvent;
         private TcpClient _baseClient;
-        private CancellationToken _globalCancellationToken;
-        private AsyncProducerConsumerQueue<MsfTask> _waitingTasks = new AsyncProducerConsumerQueue<MsfTask>();
+        private readonly CancellationTokenSource _globalCancellationTokenSource;
+        private readonly AsyncProducerConsumerQueue<MsfTask> _waitingTasks = new AsyncProducerConsumerQueue<MsfTask>();
 
+        public event EventHandler ConnectionFailed;
+        
         public long QQNumber { get; }
-
         public AccountAuthInfo AuthInfo { get; set; }
-
         public MsfGeneralInfo MsfInfo { get; }
-
         public byte[] CurrentCookie { get; private set; }
 
         public MsfServer(long qqNumber, MsfGeneralInfo msfInfo, AccountAuthInfo auth = null)
@@ -62,87 +39,141 @@ namespace SharpQQ.Protocol.Msf
             this.QQNumber = qqNumber;
             this.AuthInfo = auth;
             this.MsfInfo = msfInfo;
+            this._globalCancellationTokenSource = new CancellationTokenSource();
+            this._connectedEvent = new AsyncManualResetEvent(false);
         }
 
         #region Sequence
+
         private int _nextSequence = 10000;
+
         public int IncreaseSequence()
         {
             return Interlocked.Increment(ref _nextSequence) - 1;
         }
+
         #endregion
 
-        public Task<MsfResult> DoRequest(string opName, byte[] data)
+        public Task<MsfResult> DoRequest(string opName, byte[] data, int timeout = Timeout.Infinite)
         {
-            MsfTask task = new MsfTask();
-            task.Sequence = this.IncreaseSequence();
-            task.CompletionSource = new TaskCompletionSource<MsfResult>();
-            task.OperationName = opName;
-            task.Data = data;
-            this._waitingTasks.EnqueueAsync(task);
-            return task.CompletionSource.Task;
-            /*
-             */
-        }
-
-        private byte[] EncodeRequest(MsfTask task)
-        {
-            RequestHead head = new RequestHead();
-            head.Sequence = task.Sequence;
-            head.AppId = this.MsfInfo.AppId;
-            head.NetworkType = this.MsfInfo.NetworkType;
-            if (this.AuthInfo?.A2 != null)
-                head.A2 = this.AuthInfo.A2;
-            else
-                head.A2 = new byte[0];
-
-            head.OperationName = task.OperationName;
-            head.Cookie = this.CurrentCookie;
-            head.IMEI = this.MsfInfo.IMEI;
-            head.IMSIRevision = this.MsfInfo.IMSIRevision;
-           head.KSID = this.MsfInfo.KSID;
-            SsoRequestContent content = new SsoRequestContent();
-            content.Head = head;
-            content.Payload = task.Data;
-            SsoRequest req = new SsoRequest();
-            if (NoEncryptionOperations.Contains(task.OperationName))
+            var task = new MsfTask
             {
-                req.EncryptionType = SsoEncryptionType.NotEncrypted;
-                req.D2 = new byte[0];
-                req.EncryptedContent = content.GetBinary();
-                req.
+                Sequence = this.IncreaseSequence(),
+                CompletionSource = new TaskCompletionSource<MsfResult>(),
+                OperationName = opName,
+                Data = data
+            };
+            this._waitingTasks.EnqueueAsync(task);
+            _taskList.Add(task.Sequence, task);
+            if (timeout != Timeout.Infinite)
+            {
+                var source = new CancellationTokenSource(timeout);
+                source.Token.Register(() =>
+                {
+                    task.CompletionSource.TrySetCanceled(source.Token);
+                    if (_taskList.ContainsKey(task.Sequence))
+                        _taskList.Remove(task.Sequence);
+                }, false);
             }
+
+            return task.CompletionSource.Task;
         }
+
 
         private async Task SendLoop()
         {
-            while (!_globalCancellationToken.IsCancellationRequested)
+            while (!_globalCancellationTokenSource.Token.IsCancellationRequested)
             {
-                if (!this.Connected)
-                {
-                    await this.ConnectAsync();
-                }
+                await _connectedEvent.WaitAsync(_globalCancellationTokenSource.Token);
+                var task = await this._waitingTasks.DequeueAsync(_globalCancellationTokenSource.Token);
+
                 try
                 {
-                    var task = await this._waitingTasks.DequeueAsync();
-
+                    var rawData = SsoHelper.EncodeRequest(this.IncreaseSequence(), this.QQNumber, task.OperationName,
+                        task.Data, this.CurrentCookie, this.MsfInfo, this.AuthInfo);
+                    await this._baseClient.WritePacketAsync(rawData);
                 }
-                catch (Exception e)
+                catch (IOException ex)
                 {
-
+                    // Network error occurred, push it to queue again.
+                    await this._waitingTasks.EnqueueAsync(task, _globalCancellationTokenSource.Token);
+                    HandleConnectionFailure();
+                    continue;
+                }
+                catch (Exception ex) // Other exceptions: throw.
+                {
+                    task.CompletionSource.TrySetException(ex);
+                    continue;
                 }
             }
         }
 
-        private async Task ConnectAsync()
+        private async Task ReceiveLoop()
         {
-            if (this._baseClient != null)
+            while (!_globalCancellationTokenSource.Token.IsCancellationRequested)
             {
-                this._baseClient.Dispose();
+                await _connectedEvent.WaitAsync(_globalCancellationTokenSource.Token);
+                try
+                {
+                    var packet = await this._baseClient.ReadPacketAsync();
+                    var content = SsoHelper.DecodeResponse(packet, this.AuthInfo);
+                    if (_taskList.ContainsKey(content.Header.Sequence))
+                    {
+                        var result = new MsfResult()
+                        {
+                            ErrorMessage = content.Header.ErrorMessage,
+                            ReturnCode = content.Header.ReturnCode,
+                            ResponsePayload = content.Payload
+                        };
+                        int taskId = content.Header.Sequence;
+                        _taskList[taskId].CompletionSource.TrySetResult(result);
+                        _taskList.Remove(taskId);
+                    }
+                }
+                catch (IOException ex)
+                {
+                    HandleConnectionFailure();
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    // TODO: Implement error handling.
+                    Console.WriteLine($"Warning: unhandled exception in msf receive loop. {ex.ToString()}");
+                }
             }
+        }
+
+        private void HandleConnectionFailure()
+        {
+            this._baseClient?.Dispose();
+            this._connectedEvent.Reset();
+            this.ConnectionFailed?.Invoke(this, new EventArgs());
+        }
+
+        public async Task ConnectAsync(int defaultTimeout = 5000)
+        {
+            this._baseClient?.Dispose();
+            
             this._baseClient = new TcpClient();
-            await this._baseClient.ConnectAsync(DefaultAddress, DefaultPort);
-            this.CurrentCookie = Utils.BinaryUtils.UnifiedRandomBytes(4);
+            this.CurrentCookie = BinaryUtils.UnifiedRandomBytes(4);
+
+            try
+            {
+                await this._baseClient.ConnectAsync(DefaultAddress, DefaultPort);
+                var cts = new CancellationTokenSource(defaultTimeout);
+                cts.Token.Register(() => this._baseClient.Dispose(), false);
+                await this._baseClient.WritePacketAsync(new MsfNegotiationPacket().GetBinary());
+                var response = await this._baseClient.ReadPacketAsync();
+                var receivedNegotiationPacket = new MsfNegotiationPacket();
+                receivedNegotiationPacket.ParseFrom(response);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw new Exception("Unable to establish a connection to MSF server.", ex);
+            }
+            
+            this._connectedEvent.Set();
         }
     }
 }
---
+++ SharpQQLib/Protocol/Msf/Packets/MsfNegotiationPacket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using SharpQQ.Binarizer.Structured;
+
+namespace SharpQQ.Protocol.Msf.Packets
+{
+    public class MsfNegotiationPacket : StructuredBinaryPacket
+    {
+        private const int DefaultVersion = 20140601;
+        private const string DefaultMsfString = "MSF";
+        
+        [IntegerField(1)]
+        public int Version { get; set; } = DefaultVersion;
+
+        [IntegerField(2)]
+        public int _AlwaysZero1 { get; set; } = 0;
+
+        [VariableLengthByteArrayField(3, PrependLengthType.Int8, PrependLengthTransform.NoTransform)]
+        public string MsfString { get; set; } = DefaultMsfString;
+
+        [IntegerField(4)]
+        public byte _AlwaysZero2 { get; set; } = 0;
+
+        [IntegerField(5)]
+        public int _AlwaysZero3 { get; set; } = 0;
+
+        public override void Check()
+        {
+            if (Version != DefaultVersion || MsfString != DefaultMsfString)
+            {
+                throw new Exception($"Malformed MSF negotiation packet; maybe not an MSF server.");
+            }
+        }
+    }
+}
