2bac9a70cce554516349d61cb6dd5e18310ad64a
t123yh <t123yh@outlook.com>
2018-01-07 23:21:34 +0800

    Add connection test.

--- Tests/MsfServerTests.cs
+++ Tests/MsfServerTests.cs
@@ -1,13 +1,29 @@
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpQQ.Protocol.Msf;
 
 namespace Tests
 {
     [TestClass]
     public class MsfServerTest
     {
-        [TestMethod]
-        public void TestConnect()
+        private static readonly byte[] KSID = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        private static readonly MsfGeneralInfo GlobalMsfInfo = new MsfGeneralInfo()
         {
+            AppId = 537051018,
+            IMEI = "867981879491101",
+            IMSIRevision = "460110500720615|A1.1.5.9114",
+            KSID = KSID,
+            NetworkType = 1
+        };
+        
+
+        [TestMethod]
+        public async Task TestConnect()
+        {
+            var server = new MsfServer(2260128230, GlobalMsfInfo, null);
+            await server.ConnectAsync();
         }
     }
 }
