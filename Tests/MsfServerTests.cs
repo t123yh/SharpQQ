d571986b30a72ea030aad683bdf026cb91b091d3
t123yh <t123yh@outlook.com>
2018-01-07 22:52:45 +0800

    Add unit tests.

---
+++ Tests/MsfServerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class MsfServerTest
+    {
+        [TestMethod]
+        public void TestConnect()
+        {
+        }
+    }
+}
