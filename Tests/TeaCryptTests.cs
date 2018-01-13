d7a5d4b28c4f69eb4991822be2e33c216c4a6767
t123yh <t123yh@outlook.com>
2018-01-13 22:23:44 +0800

    Add unit test for TeaCrypt.

---
+++ Tests/TeaCryptTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpQQ;
+using SharpQQ.Utils;
+
+namespace Tests
+{
+    [TestClass]
+    public class TeaCryptTests
+    {
+        private readonly byte[] Data = Encoding.ASCII.GetBytes("TooYoung");
+        private readonly byte[] Key = Encoding.UTF8.GetBytes("Sometimes naïve"); // 16 bytes long because `ï` is 2 bytes
+        private readonly byte[] Cipher = BinaryUtils.HexToBin("774522845C2D529A");
+        
+        [TestMethod]
+        public void TestEncrypt()
+        {
+            var encrypted = TeaCrypt.EncryptBlock(Data, Key);
+            CollectionAssert.AreEqual(encrypted, Cipher);
+        }
+
+        [TestMethod]
+        public void TestDecrypt()
+        {
+            var decrypted = TeaCrypt.DecryptBlock(Cipher, Key);
+            CollectionAssert.AreEqual(decrypted, Data);
+        }
+    }
+}
