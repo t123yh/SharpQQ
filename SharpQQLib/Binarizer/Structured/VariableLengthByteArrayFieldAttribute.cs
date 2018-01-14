6ce5dfd6e85e34987753804f5c7c705a414a8e63
t123yh <t123yh@outlook.com>
2018-01-14 12:12:43 +0800

    Add max length support for VariableLengthByteArray.

--- SharpQQLib/Binarizer/Structured/VariableLengthByteArrayFieldAttribute.cs
+++ SharpQQLib/Binarizer/Structured/VariableLengthByteArrayFieldAttribute.cs
@@ -1,27 +1,44 @@
 using System;
+using System.Linq;
 using System.Text;
 
 namespace SharpQQ.Binarizer.Structured
 {
     public class VariableLengthByteArrayFieldAttribute : VariableLengthFieldAttribute
     {
+        public int MaxLength { get; set; }
         public static readonly Encoding DefaultTextEncoding = Encoding.UTF8;
 
-        public VariableLengthByteArrayFieldAttribute(int index, PrependLengthType type, PrependLengthTransform transform = PrependLengthTransform.NoTransform) : base(index, type, transform)
+        public VariableLengthByteArrayFieldAttribute(int index, PrependLengthType type,
+            PrependLengthTransform transform = PrependLengthTransform.NoTransform, int maxLength = -1) : base(index,
+            type, transform)
         {
+            this.MaxLength = maxLength;
+        }
+
+        private bool CheckLength(int len)
+        {
+            return (MaxLength < 0 || len <= MaxLength);
         }
 
         protected override byte[] ConvertToByteArray(object val)
         {
+            byte[] result;
             if (val is string str)
-                return DefaultTextEncoding.GetBytes(str);
+                result = DefaultTextEncoding.GetBytes(str);
             else if (val is byte[] ba)
-                return ba;
-            throw new ArgumentException($"Parameter must be a byte array instead of {val.GetType().ToString()}.");
+                result = ba;
+            else
+                throw new ArgumentException($"Parameter must be a byte array instead of {val.GetType().ToString()}.");
+
+            return CheckLength(result.Length) ? result : result.Take(MaxLength).ToArray();
         }
 
         protected override object ParseFromByteArray(Type targetType, byte[] buf)
         {
+            if (!CheckLength(buf.Length))
+                buf = buf.Take(MaxLength).ToArray();
+
             if (targetType == typeof(string))
                 return DefaultTextEncoding.GetString(buf);
             else if (targetType == typeof(byte[]))
