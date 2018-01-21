06bacc1f04ce5f96ff5c1406856bbe503a0717af
t123yh <t123yh@outlook.com>
2018-01-21 17:20:22 +0800

    Add PacketName and OS in constants.

--- SharpQQLib/Constants.cs
+++ SharpQQLib/Constants.cs
@@ -3,11 +3,15 @@ namespace SharpQQ
     public class Constants
     {
         public const int AppId = 537051018;
+        
+        public const string PacketName = "com.tencent.tim";
 
         public const string AppVersion = "1.1.5.9114";
 
         public const string AndroidVersion = "7.1.2";
 
         public const string Operator = "中国电信";
+
+        public const string OS = "android";
     }
 }
