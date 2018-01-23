d64233719e2bf62c17cda93ad11e705963b3f7a3
t123yh <t123yh@outlook.com>
2018-01-23 22:51:05 +0800

    Add Exceptions to Service.

---
+++ SharpQQLib/Service/Exceptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SharpQQ.Service
+{
+    public class QQException : Exception
+    {
+        public QQException()
+        {
+        }
+
+        public QQException(string message) : base(message)
+        {
+        }
+
+        public QQException(string message, Exception innerException)
+        {
+        }
+    }
+
+    public class LoginException : Exception
+    {
+        public int Code { get; }
+
+        public LoginException(string message, int code) : base(message)
+        {
+            this.Code = code;
+        }
+    }
+}
