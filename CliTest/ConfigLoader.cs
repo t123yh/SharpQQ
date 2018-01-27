9a8d88808bf68c8c15e41ecc3d6e46fb9728e48e
t123yh <t123yh@outlook.com>
2018-01-27 19:16:11 +0800

    Add Config files.

---
+++ CliTest/ConfigLoader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace CliTest
+{
+    public static class ConfigLoader
+    {
+        public static async Task<DeviceIdentity> LoadDeviceIdentity(string fileName)
+        {
+            DeviceIdentity result;
+            try
+            {
+                var val = await File.ReadAllTextAsync(fileName);
+
+                var deserializer = new DeserializerBuilder()
+                    .WithNamingConvention(new PascalCaseNamingConvention())
+                    .Build();
+                result = deserializer.Deserialize<DeviceIdentity>(new StringReader(val));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to read device identity file, generating a new one...");
+                result = await DeviceIdentity.AskForInfo();
+                var serializer = new SerializerBuilder()
+                    .WithNamingConvention(new PascalCaseNamingConvention())
+                    .Build();
+
+                var text = serializer.Serialize(result);
+                await File.WriteAllTextAsync(fileName, text);
+                Console.WriteLine($"Device identity has been saved to {fileName}");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CliTest/DeviceIdentity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CliTest
+{
+    public class DeviceIdentity
+    {
+        public string IMEI { get; set; }
+        public string IMSI { get; set; }
+        public string AndroidVersion { get; set; }
+        public string NetworkOperator { get; set; }
+        public string DeviceId { get; set; }
+
+        public static async Task<DeviceIdentity> AskForInfo()
+        {
+            var instance = new DeviceIdentity();
+            
+            Console.WriteLine("Device information can be generated here: http://www.myfakeinfo.com/mobile/get-android-device-information.php");
+            instance.IMEI = (await Utils.PromptUntil("IMEI: ", Utils.ValidateIMEI, "Invalid IMEI, please generate one")).Trim();
+            instance.IMSI = (await Utils.PromptUntil("IMSI (You'd better use a Chinese IMSI): ", Utils.ValidateIMSI)).Trim();
+
+            instance.AndroidVersion = (await Utils.PromptUntil("Android version (e.g. 6.0.0): ", Utils.ValidateAndroidVersion)).Trim();
+            instance.NetworkOperator = (await Utils.Prompt("Network operator (e.g. 中国电信): "));
+            
+            byte[] deviceId = new byte[16];
+            Utils.GlobalRandom.NextBytes(deviceId);
+            instance.DeviceId = SharpQQ.Utils.BinaryUtils.ToHex(deviceId);
+
+            return instance;
+        }
+    }
+}
